2dce0ac5d23c24c719c08e525261a435f4a91a4e
Spare <Spare@Heroku.com>
2024-05-12 15:44:47 +0700

    v2

--- src/routes/absenManual.tsx
+++ src/routes/absenManual.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Side from "../components/sidebar";
 import Camera from "react-html5-camera-photo";
 import "react-html5-camera-photo/build/css/index.css";
-// import axios from "axios";
+import axios from "axios";
 // import * as Realm from "realm-web";
 
 // const APP_ID = "data-wzvck";
@@ -13,6 +13,7 @@ import "react-html5-camera-photo/build/css/index.css";
 
 export default function AbsenManual() {
   const [image, setImage] = useState<string | null>(null);
+  const [dataRequest, setDataRequest] = useState<any>(null);
   const [facingMode, setFacingMode] = useState<"environment" | "user">(
     "environment"
   );
@@ -23,49 +24,37 @@ export default function AbsenManual() {
   };
 
   const handleTakePhoto = (dataUri: string) => {
+    const imageData = dataUri.replace(
+      /^data:image\/(png|jpg|jpeg);base64,/,
+      ""
+    );
     setImage(dataUri);
-    console.log(dataUri);
+    const binaryString = atob(imageData);
+    const bytes = new Uint8Array(binaryString.length);
+    for (let i = 0; i < binaryString.length; i++) {
+      bytes[i] = binaryString.charCodeAt(i);
+    }
+    // console.log(bytes);
+    sendImageDataToServer(bytes);
   };
 
-  // const handleUploadImage = (e: ChangeEvent<HTMLInputElement>) => {
-  //   const file = e.target.files && e.target.files[0];
-  //   if (file) {
-  //     const reader = new FileReader();
-  //     reader.onloadend = () => {
-  //       if (reader.readyState === FileReader.DONE) {
-  //         const arrayBuffer = reader.result as ArrayBuffer;
-  //         const uint8Array = new Uint8Array(arrayBuffer);
-  //         sendImageDataToServer(uint8Array);
-  //       }
-  //     };
-  //     reader.readAsArrayBuffer(file);
-  //   }
-  // };
-
-  // const sendImageDataToServer = async (imageData: Uint8Array) => {
-  //   try {
-  //     const url = "http://192.168.0.3:8080/test/";
-  //     const data = {
-  //       gambar: imageData,
-  //     };
-  //     const headers = {
-  //       apikey:
-  //         "2xyduuL3bxdCUTekwPRLdHlIeq3t5hHhKi62RKlkMQbH7BTHNGImZjpNxrdHme61",
-  //     };
-  //     const res = await axios.post(url, data, { headers });
-  //     console.log(res.data);
-  //   } catch (error) {
-  //     console.error("Error uploading image:", error);
-  //   }
-  // };
+  const sendImageDataToServer = async (blob: any) => {
+    try {
+      const res = await axios.post(
+        "https://api.dprdbekasi.cloud/absensi/",
+        blob
+      );
+      console.log(res.data);
+      setDataRequest(res.data);
+    } catch (error) {
+      console.error("Error sending image data to server:", error);
+    }
+  };
 
   return (
     <>
       <Side>
         <div className="pt-16 lg:pt-0 max-md:text-xs text-base gap-10">
-          {/* <div className="flex justify-center items-center bg-red-500 max-md:pr-6 pt-20"> */}
-          {/* className="max-md:text-xs text-base h-[70dvh] max-md:w-[80dvw] rounded-lg border border-gray-200 p-4" */}
-          {/* <div className="max-md:text-xs text-base h-[70dvh] max-md:w-[80dvw] rounded-lg border border-gray-200 p-4"> */}
           <div className="flex flex-col items-center justify-between rounded-lg border border-gray-200 p-4 mr-6">
             <h1 className="font-bold mb-4">Absen Manual</h1>
             <div className="w-[50dvh]">
@@ -73,6 +62,7 @@ export default function AbsenManual() {
                 <img src={image} alt="Uploaded" />
               ) : (
                 <Camera
+                  imageType="jpg"
                   idealFacingMode={facingMode}
                   idealResolution={{ width: 480, height: 640 }}
                   isFullscreen={false}
@@ -89,6 +79,20 @@ export default function AbsenManual() {
                 <p>Ganti Kamera</p>
               </button>
             </div>
+            {dataRequest && dataRequest[0] !== "ANOMALI" && "Tidak Terbaca" ? (
+              <div className="pt-8 pb-2">
+                <p className="text-xl">
+                  Hallo {dataRequest[0]} Anda Sudah Absen
+                </p>
+              </div>
+            ) : (
+              dataRequest &&
+              dataRequest[0] === "ANOMALI" && (
+                <div className="pt-8 pb-2">
+                  <p className="text-xl">Wajah Tidak Terdekteksi</p>
+                </div>
+              )
+            )}
           </div>
         </div>
       </Side>
